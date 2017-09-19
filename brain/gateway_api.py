03dce44a467d311c147eb200e9d6256a07c24ee7
Ryan Padilha <ryan.padilha@gmail.com>
2017-09-19 18:02:40 -0300

    ad production

--- brain/gateway_api.py
+++ brain/gateway_api.py
@@ -8,25 +8,25 @@ from flask_restful import Api
 
 
 def create_app(mode):
-    instance_path = path.join(
-        path.abspath(path.dirname(__file__)), "%s_instance" % mode
-    )
+    ## instance_path = path.join(
+    ##    path.abspath(path.dirname(__file__)), "%s_instance" % mode
+    ## )
 
-    app = Flask('brain',
-                instance_path=instance_path,
-                instance_relative_config=True)
+    app = Flask('brain')
+                ## instance_path=instance_path,
+                ## instance_relative_config=True)
 
     # read constants of the current file
     # app.config.from_object(__name__)
 
     # read constants from module, absolute path
-    app.config.from_object('brain.default_settings')
+    ## app.config.from_object('brain.default_settings')
 
     # read constants from system-environment
     # app.config.from_envvar(config_env_var, silent=False)
 
     # read constants from python-file
-    app.config.from_pyfile('config.cfg')
+    ## app.config.from_pyfile('config.cfg')
 
     # read constants from json-file
     # app.cofig.update(**extra_config)
--- wsgi.py
+++ wsgi.py
@@ -2,9 +2,7 @@
 
 # entry point for the application based on WSGI
 
-import sys
-from brain.gateway_api import create_app
+from run import application
 
-mode = 'production'
-application = create_app(mode=mode)
-application.run(**application.config.get_namespace('RUN_'))
+if __name__ == '__main__':
+    application.run()
